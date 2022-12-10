adc0f9119c61b633f6d9121e62e184d1d920e3e2
VineethHamilpur590 <115372818+VineethHamilpur590@users.noreply.github.com>
2022-12-09 17:37:51 -0800

    Update dbconnect.php

--- dbconnect.php
+++ dbconnect.php
@@ -9,7 +9,14 @@ $password = "password";
 
 $dbname = "iphoneproductsdb";
 
-$conn = new MySQLi($servername, $username, $password, $dbname);
+try {
+  $conn = new PDO("mysql:host=$servername;dbname=myDB", $username, $password);
+  // set the PDO error mode to exception
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  echo "connected sucessfully";
+} catch(PDOException $e) {
+  echo "Connection failed: " . $e->getMessage();
+}
 
 
 ?>
