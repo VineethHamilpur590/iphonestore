72c9451e03263f9ff590da837ddf14644f0f8948
VineethHamilpur590 <115372818+VineethHamilpur590@users.noreply.github.com>
2022-12-09 18:09:44 -0800

    Update dbconnect.php

--- dbconnect.php
+++ dbconnect.php
@@ -1,26 +1,15 @@
-
-
-
-
 <?php
 $servername = "iphonedb.cd8it5oofcje.us-west-1.rds.amazonaws.com";
 $username = "root";
 $password = "password";
 
+// for local machine password=> Password@123
+// for remote
+//$password = "root";
+
 $dbname = "iphoneproductsdb";
 
-try{
-// Create connection
-$conn = mysqli($servername, $username, $password,$dbname);
+$conn = new mysqli($servername, $username, $password, $dbname);
 
-// Check connection
-if (!$conn) {
-  die("Connection failed: " . mysqli->errror);
-}
-echo "Connected successfully";
-
-}catch(Exception $e){
-  echo $e;
-}
 
 ?>
